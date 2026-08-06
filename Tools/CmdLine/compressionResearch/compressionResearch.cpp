95d6a10aaeda16ae597948237be154fccc3cb3d3
ChrisCoxArt <ccox@comcast.net>
2026-08-06 15:50:10 -0700

    previous 3D

--- Tools/CmdLine/compressionResearch/compressionResearch.cpp
+++ Tools/CmdLine/compressionResearch/compressionResearch.cpp
@@ -211,6 +211,8 @@ static func_predictor max3D_forward;
 static func_predictor max3D_reverse;
 static func_predictor median3D_forward;
 static func_predictor median3D_reverse;
+static func_predictor prev3D_forward;
+static func_predictor prev3D_reverse;
 
 // utilities
 static void bytesplit16( const uint8_t *input, uint8_t *output, size_t count );
@@ -379,11 +381,14 @@ std::vector<predictor_desc> predictorList =
  { "MinGradSplitChan", PREDICTOR_TYPE_2D, false, splitchannelswrap<MinGrad_forward>, unsplitchannelswrap<MinGrad_reverse> },
 
 
+ { "Prev3D", PREDICTOR_TYPE_3D, false, prev3D_forward, prev3D_reverse },
  { "Min3D", PREDICTOR_TYPE_3D, false, min3D_forward, min3D_reverse },
  { "Max3D", PREDICTOR_TYPE_3D, false, max3D_forward, max3D_reverse },
  { "Median3D", PREDICTOR_TYPE_3D, false, median3D_forward, median3D_reverse },
 
 // splits should only be used if depth > 8
+ { "Prev3DSplit", PREDICTOR_TYPE_3D, false, splitwrap<prev3D_forward>, unsplitwrap<prev3D_reverse> },
+ { "Prev3DSplitChan", PREDICTOR_TYPE_3D, false, splitchannelswrap<prev3D_forward>, unsplitchannelswrap<prev3D_reverse> },
  { "Min3DSplit", PREDICTOR_TYPE_3D, false, splitwrap<min3D_forward>, unsplitwrap<min3D_reverse> },
  { "Min3DSplitChan", PREDICTOR_TYPE_3D, false, splitchannelswrap<min3D_forward>, unsplitchannelswrap<min3D_reverse> },
  { "Max3DSplit", PREDICTOR_TYPE_3D, false, splitwrap<max3D_forward>, unsplitwrap<max3D_reverse> },
@@ -2912,6 +2917,156 @@ void MinGrad_reverse( const uint8_t *input, uint8_t *output, int bitDepth, int c
         }
       }  // end x loop
     }   // end y loop
+  } // end 16 bit
+
+}
+
+/******************************************************************************/
+
+static
+void prev3D_forward( const uint8_t *input, uint8_t *output, int bitDepth, int channels,
+                size_t size1, size_t size2, size_t size3,
+                size_t colStep, size_t rowStep, size_t planeStep )
+{
+  if (bitDepth == 8) {
+    // 2D first plane
+    prev2D_forward( input, output, 8, channels, size1, size2, 0, colStep, rowStep, 0 );
+
+    for (size_t z = 1; z < size3; ++z) {
+      const uint8_t *inZ = input + z*planeStep;
+      const uint8_t *belowZ = input + (z-1)*planeStep;
+      uint8_t *outZ = output + z*planeStep;
+
+      // previous rows
+      for (size_t y = 0; y < size2; ++y) {
+        const uint8_t *inY = inZ + y*rowStep;
+        const uint8_t *belowY = belowZ + y*rowStep;
+        uint8_t *outY = outZ + y*rowStep;
+        
+        // below first pixel
+        for (int c = 0; c < channels; ++c) {
+            outY[c] = inY[c] - belowY[c];   // overflow/underflow is intentional
+        }
+        for (size_t x = 1; x < size1; ++x) {
+          const uint8_t *inX = inY + x*colStep;
+          const uint8_t *prevX = inY + (x-1)*colStep;
+          uint8_t *outX = outY + x*colStep;
+          
+          for (int c = 0; c < channels; ++c) {
+            outX[c] = inX[c] - prevX[c];   // overflow/underflow is intentional
+          }
+        }  // end x loop
+      }   // end y loop
+    } // end z loop
+  } // end 8 bit
+
+  if (bitDepth == 16) {
+    // 2D first plane
+    prev2D_forward( input, output, 16, channels, size1, size2, 0, colStep, rowStep, 0 );
+    
+    uint16_t *input16 = (uint16_t*)input;
+    uint16_t *output16 = (uint16_t*)output;
+    for (size_t z = 1; z < size3; ++z) {
+      const uint16_t *inZ = input16 + z*planeStep;
+      const uint16_t *belowZ = input16 + (z-1)*planeStep;
+      uint16_t *outZ = output16 + z*planeStep;
+
+      // previous rows
+      for (size_t y = 0; y < size2; ++y) {
+        const uint16_t *inY = inZ + y*rowStep;
+        const uint16_t *belowY = belowZ + y*rowStep;
+        uint16_t *outY = outZ + y*rowStep;
+        
+        // below first pixel
+        for (int c = 0; c < channels; ++c) {
+            outY[c] = inY[c] - belowY[c];   // overflow/underflow is intentional
+        }
+        for (size_t x = 1; x < size1; ++x) {
+          const uint16_t *inX = inY + x*colStep;
+          const uint16_t *prevX = inY + (x-1)*colStep;
+          uint16_t *outX = outY + x*colStep;
+          
+          for (int c = 0; c < channels; ++c) {
+            outX[c] = inX[c] - prevX[c];   // overflow/underflow is intentional
+          }
+        }  // end x loop
+      }   // end y loop
+    } // end z loop
+  } // end 16 bit
+
+}
+
+/******************************************************************************/
+
+static
+void prev3D_reverse( const uint8_t *input, uint8_t *output, int bitDepth, int channels,
+                size_t size1, size_t size2, size_t size3,
+                size_t colStep, size_t rowStep, size_t planeStep )
+{
+  if (bitDepth == 8) {
+    // 2D first plane
+    prev2D_reverse( input, output, 8, channels, size1, size2, 0, colStep, rowStep, 0 );
+
+    for (size_t z = 1; z < size3; ++z) {
+      const uint8_t *inZ = input + z*planeStep;
+      const uint8_t *belowZ = output + (z-1)*planeStep;
+      uint8_t *outZ = output + z*planeStep;
+
+      // previous rows
+      for (size_t y = 0; y < size2; ++y) {
+        const uint8_t *inY = inZ + y*rowStep;
+        const uint8_t *belowY = belowZ + y*rowStep;
+        uint8_t *outY = outZ + y*rowStep;
+        
+        // below first pixel
+        for (int c = 0; c < channels; ++c) {
+            outY[c] = inY[c] + belowY[c];   // overflow/underflow is intentional
+        }
+        for (size_t x = 1; x < size1; ++x) {
+          const uint8_t *inX = inY + x*colStep;
+          const uint8_t *prevX = outY + (x-1)*colStep;
+          uint8_t *outX = outY + x*colStep;
+          
+          for (int c = 0; c < channels; ++c) {
+            outX[c] = inX[c] + prevX[c];   // overflow/underflow is intentional
+          }
+        }  // end x loop
+      }   // end y loop
+    } // end z loop
+  } // end 8 bit
+
+  if (bitDepth == 16) {
+    // 2D first plane
+    prev2D_reverse( input, output, 16, channels, size1, size2, 0, colStep, rowStep, 0 );
+    
+    uint16_t *input16 = (uint16_t*)input;
+    uint16_t *output16 = (uint16_t*)output;
+    for (size_t z = 1; z < size3; ++z) {
+      const uint16_t *inZ = input16 + z*planeStep;
+      const uint16_t *belowZ = output16 + (z-1)*planeStep;
+      uint16_t *outZ = output16 + z*planeStep;
+    
+      // previous rows
+      for (size_t y = 0; y < size2; ++y) {
+        const uint16_t *inY = inZ + y*rowStep;
+        const uint16_t *belowY = belowZ + y*rowStep;
+        uint16_t *outY = outZ + y*rowStep;
+        
+        // below first pixel
+        for (int c = 0; c < channels; ++c) {
+            outY[c] = inY[c] + belowY[c];   // overflow/underflow is intentional
+        }
+        for (size_t x = 1; x < size1; ++x) {
+          const uint16_t *inX = inY + x*colStep;
+          const uint16_t *prevX = outY + (x-1)*colStep;
+          uint16_t *outX = outY + x*colStep;
+          
+          for (int c = 0; c < channels; ++c) {
+            outX[c] = inX[c] + prevX[c];   // overflow/underflow is intentional
+          }
+        }  // end x loop
+      }   // end y loop
+    } // end z loop
   } // end 16 bit
 
 }
