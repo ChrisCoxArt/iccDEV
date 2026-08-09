2afc9c1d09df693ed1119361c0e2676a9ce79829
David Hoyt <dhoyt@hoyt.net>
2026-08-08 21:36:52 -0400

    Ci qa latest release packaging (#2055)

--- matlab/build_mex.m
+++ matlab/build_mex.m
@@ -195,7 +195,15 @@ function [linkArgs, runtimeFiles] = find_optional_dependencies( ...
   if ~isempty(tripletMatch)
     triplet = strtrim(tripletMatch{1});
   end
-  tripletRoot = fullfile(buildDir, 'vcpkg_installed', triplet);
+  cachedInstalledRoot = '';
+  installedMatch = regexp(cacheText, ...
+    '(?m)^VCPKG_INSTALLED_DIR:[^=]*=([^\r\n]+)\s*$', 'tokens', 'once');
+  if ~isempty(installedMatch)
+    cachedInstalledRoot = strtrim(installedMatch{1});
+  end
+  installedRoot = iccdev.qa.select_vcpkg_installed_root( ...
+    buildDir, cachedInstalledRoot, triplet);
+  tripletRoot = fullfile(installedRoot, triplet);
   staticTriplet = ~isempty(strfind(triplet, '-static')); %#ok<STREMP>
   if debugBuild
     libraryCandidates = {
